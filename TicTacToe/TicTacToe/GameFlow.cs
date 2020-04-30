41f09486e6c877d4aa7b7eeef0d48a7460b73df2
Lee Yun Hwan <zoo7930@naver.com>
2020-04-30 19:24:54 +0900

    Integer exception handling

--- TicTacToe/TicTacToe/GameFlow.cs
+++ TicTacToe/TicTacToe/GameFlow.cs
@@ -7,20 +7,13 @@ using System.Threading.Tasks;
 namespace TicTacToe
 {
     class GameFlow : GameScenes
-    {
-        public enum Menu
-        {
-         PLAYERVSPLAYER,
-         PLAYEVSCOMPUTER,
-         ADDPLAYER,
-         RANKING,
-         ENDGAME   
-        }
+    {      
         public int SelectType()
         {
-            char selectedTypeInChar;
+            string selectedTypeInString;      
             int selectedType = 0;
             bool rightInput = false;
+
             List<string> menuItem = new List<string>()   //메뉴 리스트 생성
             {
                 "1. PLAYER VS PLAYER",
@@ -29,24 +22,34 @@ namespace TicTacToe
                 "4. RANKING",
                 "5. END GAME"
             };
+
             while (!rightInput)
             {
                 foreach (string item in menuItem)
                 {
                     Console.WriteLine(item);
                 }
-                Console.Write("메뉴 선택 : ");
-                selectedTypeInChar = Convert.ToChar(Console.Read());
 
-                if (selectedTypeInChar >= '1' &&  selectedTypeInChar <= '5') {
-                    selectedType = selectedTypeInChar - '0';
-                    rightInput = true;
+                Console.WriteLine("\n1~5사이의 정수를 입력하세요\n");
+
+                Console.Write("메뉴 선택 : ");
+                selectedTypeInString = Console.ReadLine();
+
+                if(selectedTypeInString.Length == 1)     //문자열의 길이가 1인지 판별 (한자리수 정수를 입력받으려 하므로)
+                {
+                    if (string.Compare(selectedTypeInString, "1") >= 0 && string.Compare(selectedTypeInString, "5") <= 0)        //입력받은 문자열의 크기가 "1" 보다크고 "5"보다 작으면 입력
+                    {
+                        selectedType = int.Parse(selectedTypeInString);
+                        rightInput = true;
+                    }
                 }
+                
                 Console.Clear();
             }
 
-            return 0;
+            return selectedType;  //입력받은 문자열 반환
         }
+
         public void SelectPlayer(ref string player1, ref string player2,int gameMode)
         {
         }
